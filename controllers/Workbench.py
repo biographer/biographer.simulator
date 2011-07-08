19b9db6ad297cf5dedede0dabda51118aea08875
marvinschulz66 <marvinschulz66@localhost>
2011-07-08 16:33:51 +0200

    Small corrections from the meeting with Matthias.

--- controllers/Workbench.py
+++ controllers/Workbench.py
@@ -2,8 +2,9 @@
 
 def index():
 	if session.bioGraph is not None:
-		if session.JSON is None or session.JSON == "":
-			session.JSON = session.bioGraph.exportJSON()
-			# export of SBML or BioPAX is not supported (yet)
+		#if session.JSON is None or session.JSON == "":
+		session.JSON = session.bioGraph.exportJSON()
+		# export of SBML or BioPAX is not supported (yet)
+		print session.JSON[:5000]
 	return dict()
 
