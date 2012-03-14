a803bf8903cd763160faa4edbed937d5755760d8
matthias bock <matthias.bock@hu-berlin.de>
2012-03-14 12:07:58 +0100

    animated coloring

--- controllers/Import.py
+++ controllers/Import.py
@@ -108,28 +108,42 @@ def BooleanNet():
 		NetworkFolder = os.path.join(request.folder, "BooleanNetworks")
 		if not os.path.exists(NetworkFolder):
 			os.mkdir(NetworkFolder)
-		return dict(AvailableNetworks=os.listdir(NetworkFolder))
+		Models = []
+		Scenarios = []
+		for entry in os.listdir(NetworkFolder):
+			s = entry.split('.')
+			extension = s[len(s)-1].lower()
+			if extension == 'boolenet':
+				Models.append(entry)
+			elif extension == 'conf':
+				Scenarios.append(entry)
+		return dict(Models=Models, Scenarios=Scenarios)
 
 	if request.env.request_method == "POST":
 		reset_current_session()
 		model = deepcopy(session.bioGraph)
 		del session.bioGraph
 
+		repository = os.path.join(request.folder, "BooleanNetworks")
+
 		if not request.vars.File in ['', None]:
-			model.importBooleanNetwork( request.vars.File.file.read() )	# upload file
+			uploaded_content = request.vars.File.file.read()
+			model.importBooleanNetwork( uploaded_content )	# upload file
+			open(repository+'/'+urllib.unquote(request.vars.file.filename), 'w').write(uploaded_content)
 		elif not request.vars.Model in ['', None]:				# choose a network on the server
-			model.importBooleanNetwork( open(os.path.join(request.folder, "BooleanNetworks", request.vars.Model)).read() )
+			model.importBooleanNetwork( open(repository+'/'+request.vars.Model).read() )
+			model.ModelName = request.vars.Model
 		else:
 			return redirect( URL(r=request,c='Import',f='BooleanNet') )
+		model.importBooleanNetworkScenarios(repository+'/'+request.vars.Scenarios)
 
-		model.importBooleanNetworkScenarios('/home/code/biographer/Whi2p.scenarios')
 		session.bioGraph = model
 
-		Layouter = request.vars.Layouter				# a Layouter was chosen
-		if Layouter == "ask":
-			return redirect( URL(r=request,c='Layout',f='choose') )
-		elif Layouter == "internal":
-			return redirect( URL(r=request,c='Layout',f='internal') )
-		else:
-			return redirect( URL(r=request,c='Layout',f='graphviz') )
+#		Layouter = request.vars.Layouter				# a Layouter was chosen
+#		if Layouter == "ask":
+#			return redirect( URL(r=request,c='Layout',f='choose') )
+#		elif Layouter == "internal":
+#			return redirect( URL(r=request,c='Layout',f='internal') )
+#		else:
+		return redirect( URL(r=request,c='Layout',f='graphviz') )
 
