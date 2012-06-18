e0940d3b14472d91ab39ff7ea6de3fbc56916a37
matthias bock <matthias.bock@hu-berlin.de>
2012-06-18 15:12:00 +0200

    merge

--- settings.js
+++ settings.js
@@ -4,7 +4,8 @@ EnableBUI	= false;
 EnableEditor	= false;
 
 env = {}
-env["biographer"]	= "https://kafka.carpe-diem-berlin.de/biographer/Plot/graphviz";
+env["biographer"]	= "http://127.0.0.1:8000/biographer";
+//env["biographer"]	= "https://kafka.carpe-diem-berlin.de/biographer/Plot/graphviz";
 env["tools"]		= "main/lib";
 env["templates"]	= "main/controls";
 env["images"]		= "img";
