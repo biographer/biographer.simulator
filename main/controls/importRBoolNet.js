259e865a620355ac8425da0c79ec8ee2afbf5ac3
Chaitanya Talnikar <chaitukca@gmail.com>
2012-06-28 20:39:17 +0530

    Made the simular OO

--- main/controls/importRBoolNet.js
+++ main/controls/importRBoolNet.js
@@ -8,36 +8,41 @@ canvas = document.getElementById('canvas');
 img = document.getElementById('Tron');
 
 function LoadMammal() {
-	delete network;
-	scopes = false;
+  
+  
 	BooleNetDebug('Downloading ...');
-	data = GET(env['biographer']+'/static/simulator/demo/mammal.r');
+  var data;
+	$.get(env['biographer']+'/static/simulator/demo/mammal.r', 
+  function(response) {
+    data = response;
+  });
+  
 	BooleNetDebug('Importing ...');
-	network = RBoolNet_Import(data);
+	var network = RBoolNet_Import(data);
 	//~ BooleNetDebug('Graphviz ...');
 	//~ doGraphviz();
 	console.log(network); 
   
-  s = graph.suspendRedraw(2000);
+  var s = graph.suspendRedraw(2000);
   
 	bui.importFromJSON(graph, network);
   //~ bui.importFromJSON(graph, example);
   
-  nodes_edges = get_nodes_edges();
+  var nodes_edges = get_nodes_edges();
   
-  bui.grid.init(nodes_edges.nodes,nodes_edges.edges);
-  bui.grid.put_on_grid();
-  bui.grid.layout();
+  //~ bui.grid.init(nodes_edges.nodes,nodes_edges.edges);
+  //~ bui.grid.put_on_grid();
+  //~ bui.grid.layout();
   
-  //~ bui.settings.straightenEdges = false;
-  //~ var cont = graph.container();
-  //~ var force = d3.layout.force()
-         //~ .charge(-4000)
-         //~ .linkDistance(100)
-         //~ .nodes(nodes_edges.nodes)
-         //~ .links(nodes_edges.edges)
-         //~ .size([$(cont).width(), $(cont).height()])
-         //~ .start();
+  bui.settings.straightenEdges = false;
+  var cont = graph.container();
+  var force = d3.layout.force()
+         .charge(-4000)
+         .linkDistance(100)
+         .nodes(nodes_edges.nodes)
+         .links(nodes_edges.edges)
+         .size([$(cont).width(), $(cont).height()])
+         .start();
   
   graph.unsuspendRedraw(s);
   
@@ -46,15 +51,7 @@ function LoadMammal() {
   
   
   simulator = new Simulator(network, graph.nodeGroup());
-  
-  //why does setAttribute not work in place of style
-  //why is so much time being spent in updating, try jquery fader solution
-  //PS: Used suspendRedraw and unsuspendRedraw to improve performance
-  //integrate layouter
-  //sbo mapping
-  //learn js classes and write better code 
-  //file i/o, initial interface
-  //removing stuff for jquery
+  $('#simulation').click(simulator.start);
   
 	if (typeof(importRBoolNetWindow) != 'undefined')
 		importRBoolNetWindow.close();
