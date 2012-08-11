c30a15d43983cfcbdd03e67a0d3e52a1f91bc6b7
Chaitanya Talnikar <chaitukca@gmail.com>
2012-08-11 22:04:53 +0530

    test cases fixed

--- test/network.py
+++ test/network.py
@@ -1,5 +1,6 @@
 from fileio import TestImportExport
 from analyse import TestAnalyse
+from selenium.webdriver.common.action_chains import ActionChains
 
 class TestNetworkGraph(TestImportExport, TestAnalyse):
   def setUp(self):
@@ -7,21 +8,35 @@ class TestNetworkGraph(TestImportExport, TestAnalyse):
     self.defaultFile()
     
   def testZoom(self):
-    pass
+    slider = self.driver.find_element_by_css_selector('.ui-slider-handle')
+    action = ActionChains(self.driver).click_and_hold(slider).move_by_offset(20, 0) \
+      .release()
+    action.perform()
     
-  def getRandomNode(self):
-    #return self.driver.find_element_by_css_selector('#graph0 > g > g:first-child > g:first-child > text')
-    return self.driver.find_element_by_css_selector('#CycD > path')
-  
+  def triggerNodeEvent(self, event):
+    selector = '#graph0 > g > g:first-child > g:first-child'
+    elem = self.driver.find_element_by_css_selector(selector)
+    action = ActionChains(self.driver)
+    
+    if event == 'click':
+      action.click(elem)
+    elif event == 'right':
+      action.context_click(elem)
+    else:
+      action.move_to_element(elem)
+      
+    action.perform()
+    
   def testNodeClick(self):
-    self.getRandomNode().click()
+    self.triggerNodeEvent('click')
     
   def testEditRule(self):
-    self.getRandomNode().context_click()
+    self.triggerNodeEvent('right')
     self.driver.find_element_by_id('buttonEdit').click()
   
   def testInfoBox(self):
-    pass
+    self.triggerNodeEvent('hover')
+    self.driver.find_element_by_id('boxInfo')
     
   def testTabTransition(self):
     self.driver.find_element_by_css_selector('#tabs > ul > li:nth-child(2)').click()
