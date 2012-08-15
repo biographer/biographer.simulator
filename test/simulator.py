bb597431f5d90cb7e78ebb93875caf57b33e71e5
Chaitanya Talnikar <chaitukca@gmail.com>
2012-08-15 15:08:53 +0530

    fixed plot x-axis tick

--- test/simulator.py
+++ test/simulator.py
@@ -6,7 +6,7 @@ class TestSimulator(unittest.TestCase):
     self.assertIsNone(self.driver.find_element_by_tag_name('body').get_attribute('JSError'))
 
   def setUp(self):
-    url = 'http://127.0.0.1:8000/biographer/static/simulator/index.html'
+    url = 'http://127.0.0.1:8000/biographer'
     self.driver = webdriver.Chrome()
     self.driver.get(url)
     self.checkJSError()
